008cc0f08fec8bed408a58a00ad3fef22a761990
nevins <nevins35@163.com>
2024-07-11 20:21:49 +0800

    feat: 添加命令

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('git-emoji-atomgit.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('git-emoji-atomgit.gitEmoji', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('git-emoji-atomgit.gitEmoji');
+	});
+
+  vscode.commands.registerCommand('git-emoji-atomgit.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from git-emoji-atomgit!');
 	});
+
+  vscode.commands.registerCommand('git-emoji-atomgit.formatSwitch', (uri?) => {
+        vscode.window.showInformationMessage('git-emoji-atomgit.formatSwitch');
+    });
 
 	context.subscriptions.push(disposable);
 }
